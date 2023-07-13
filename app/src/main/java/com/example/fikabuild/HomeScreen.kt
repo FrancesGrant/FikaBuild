d7b233bf1d6e312fc18624063333132404a0fd28
FrancesGrant <frances_grant@outlook.com>
2023-07-13 12:32:42 +0100

    Logout menu implemented on Home Screen instead of a physical button.

--- app/src/main/java/com/example/fikabuild/HomeScreen.kt
+++ app/src/main/java/com/example/fikabuild/HomeScreen.kt
@@ -2,7 +2,8 @@ package com.example.fikabuild
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
@@ -16,9 +17,15 @@ import com.google.firebase.ktx.Firebase
 class HomeScreen : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+
+    /**
+     * Overrides the `onCreate` method of the activity to initialize and set up the login screen.
+     *
+     * @param savedInstanceState The saved instance state bundle.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_screen)
 
         // Toolbar
         val toolbar = findViewById<Toolbar>(R.id.customActionBar)
@@ -26,14 +33,37 @@ class HomeScreen : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         auth = Firebase.auth
-        val logOutButton = findViewById<Button>(R.id.button)
-
-        logOutButton.setOnClickListener {
-            Firebase.auth.signOut()
-            navigateToLogin()
-        }
     }
 
+    /**
+     * Overrides the `onCreateOptionsMenu' method of the activity to initialize and set up the menu.
+     *
+     * @param menu The menu being initialized.
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add("Logout")
+        return true
+    }
+
+    /**
+     * Overrides the `onOptionsItemSelected` method of the activity to handle menu item selection.
+     *
+     * @param item The selected menu item.
+     * @return Boolean value indicating whether the item selection is handled.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.title == "Logout") {
+            Firebase.auth.signOut()
+            navigateToLogin()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Navigates the user to Login Activity.
+     *
+     */
     private fun navigateToLogin() {
         val intent = Intent(this, WelcomeScreen::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -41,3 +71,5 @@ class HomeScreen : AppCompatActivity() {
         finish()
     }
 }
+
+
